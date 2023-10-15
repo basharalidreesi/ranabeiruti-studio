b076d8202c9ad8e857f9f997253919b77236c19b
Bashar al-Idreesi <basharalidreesi@Bashars-MacBook-Pro-2.local>
2023-10-15 12:04:34 +0300

    u

--- defaultDocumentNode.js
+++ defaultDocumentNode.js
@@ -1,14 +1,32 @@
 import { EditIcon, EyeOpenIcon } from "@sanity/icons";
-import { ProjectPreview } from "./components";
+import { Iframe } from "sanity-plugin-iframe-pane";
+// import { ProjectPreview } from "./components";
+
+function resolveUrl(doc) {
+	const baseUrl = window.location.hostname === "localhost" ? "http://localhost:8080" : "";
+	// return `${baseUrl}/preview/${doc._id?.replace("drafts.", "")}/`;
+	return `${baseUrl}/preview/`;
+};
 
 export const defaultDocumentNode = (S, {schemaType}) => {
-	if (schemaType === "project") {
-		return S.document().views([
+	switch (schemaType) {
+		case "project": return S.document().views([
+			S.view.form().title("Edit"),
+			// S.view.component(Iframe).title("Preview").options({
+			// 	url: (doc) => resolveUrl(doc),
+			// 	showDisplayUrl: true,
+			// 	defaultSize: "desktop",
+			// 	reload: {
+			// 		button: true,
+			// 		revision: false,
+			// 	},
+			// 	loader: true,
+			// 	// attributes: {},
+			// }),
+			// S.view.component(ProjectPreview).title("Preview").icon(EyeOpenIcon),
+		]);
+		default: return S.document().views([
 			S.view.form().title("Edit").icon(EditIcon),
-			S.view.component(ProjectPreview).title("Preview").icon(EyeOpenIcon),
 		]);
 	};
-	return S.document().views([
-		S.view.form().title("Edit").icon(EditIcon),
-	]);
 };
--- sanity.config.js
+++ sanity.config.js
@@ -5,31 +5,42 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { dataDeskstructure, projectsDeskStructure } from "./structure";
 import { defaultDocumentNode } from "./defaultDocumentNode";
-import { DatabaseIcon, PackageIcon } from "@sanity/icons";
+import { DatabaseIcon, MoonIcon, PackageIcon, SunIcon } from "@sanity/icons";
 
-export default defineConfig({
-	name: "default",
-	title: "Rana Beiruti",
-	projectId: "rtlbcvty",
-	dataset: "production",
-	plugins: [
-		deskTool({
-			title: "Projects",
-			name: "projects",
-			icon: DatabaseIcon,
-			structure: projectsDeskStructure,
-			defaultDocumentNode: defaultDocumentNode,
-		}),
-		deskTool({
-			title: "Data",
-			name: "data",
-			icon: PackageIcon,
-			structure: dataDeskstructure,
-		}),
-		media(),
-		visionTool(),
-	],
-	schema: {
-		types: schemaTypes,
-	},
-});
+const config = (dataset) => {
+	return {
+		name: dataset,
+		projectId: "rtlbcvty",
+		dataset: dataset,
+		basePath: `/${dataset}`,
+		title: "Rana Beiruti",
+		subtitle: dataset,
+		icon: dataset === "production" ? SunIcon : MoonIcon, 
+		plugins: [
+			deskTool({
+				title: "Projects",
+				name: "projects",
+				icon: DatabaseIcon,
+				structure: projectsDeskStructure,
+				defaultDocumentNode: defaultDocumentNode,
+			}),
+			deskTool({
+				title: "Data",
+				name: "data",
+				icon: PackageIcon,
+				structure: dataDeskstructure,
+				defaultDocumentNode: defaultDocumentNode,
+			}),
+			media(),
+			visionTool(),
+		],
+		schema: {
+			types: schemaTypes,
+		},
+	};
+};
+
+export default defineConfig([
+	config("production"),
+	config("staging"),
+]);
